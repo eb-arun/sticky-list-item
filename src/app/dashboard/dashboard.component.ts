0700e809e48adb183f4772985b60e4999b517039
Arun EB <eb.arun@gmail.com>
2018-09-20 16:05:14 +0530

    Update function created, UI issues fixed

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,35 +7,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  storyTitle:any;
-  storySnap:any;
-  addBtn:boolean;
-  listData:any = [];
-  saveData:any;
+  storyTitle: any;
+  storySnap: any;
+  addBtn: boolean;
+  listData: any = [];
+  saveData: any;
+
+  updateBtn: boolean;
+  updateTitle: any;
+  updateSnap: any;
+  updateTimeMs:any;
 
   constructor() { }
 
   ngOnInit() {
   }
-  addNew () {
+  addNew() {
     this.addBtn = true;
-    this.storyTitle='';
-    this.storySnap='';
+    this.updateBtn = false;
+    this.storyTitle = '';
+    this.storySnap = '';
   }
 
   saveStory() {
     this.addBtn = false;
     this.saveData = {
-      title:this.storyTitle,
-      snap:this.storySnap,
-      created:new Date().getTime(),
-      active:true
+      title: this.storyTitle,
+      snap: this.storySnap,
+      created: new Date().getTime(),
+      updated: new Date().getTime(),
+      active: true
     }
     this.listData.push(this.saveData);
     console.log('listData', this.listData);
   }
   deleteStory(data) {
-    this.listData = this.listData.filter((res) =>res.created !== data.created);
-
+    this.listData = this.listData.filter((res) => res.created !== data.created);
+  }
+  updatePop(data) {
+    this.updateBtn = true;
+    this.addBtn = false;
+    this.updateTitle = data.title;
+    this.updateSnap = data.snap;
+    this.updateTimeMs = data.created;
+    window.scrollTo(0, 70);
+  }
+  updateStory() {
+    this.updateBtn = false;
+    this.listData.forEach(res => {
+      if (res.created == this.updateTimeMs) {
+        res.title = this.updateTitle;
+        res.snap = this.updateSnap;
+        res.updated = new Date().getTime(); 
+      }
+    })
+  }
+  cancelPop() {
+    this.addBtn = false;
+    this.updateBtn = false;
   }
 }
